76be20e265c3a95cb9402acad2acf8fdca6fe812
Andrea Viviani <andrea.viviani89@gmail.com>
2022-05-12 10:41:02 +0200

    test clening data

--- utilities.py
+++ utilities.py
@@ -1,6 +1,11 @@
 # Dependencies
+import pandas as pd
+import numpy as np
+import time
 import matplotlib.pyplot as plt
 
+from sklearn.utils._testing import ignore_warnings
+from sklearn.exceptions import ConvergenceWarning
 from sklearn.pipeline import Pipeline
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
@@ -64,3 +69,42 @@ def pipelines(models):
     ]) for name, model in models.items()}
 
     return pipes
+
+# Model performance
+@ignore_warnings(category=ConvergenceWarning)
+def perfomance(pipes, X_train, y_train, X_test, y_test):
+    """Compute mean and std of cross validation scores, accuracy on test set
+       as well as training and predicting time
+    Args: pipes(dict); as defined in `pipelines` function.
+          X_train, y_train; training sets
+          X_test, y_test; test sets
+    Returns:
+        Pandas Dataframe: Dataframe of computed performance metrics sorted by accuracy on test set
+    """
+    results = pd.DataFrame()
+
+    for name, model in pipes.items():
+
+        # training time
+        t0 = time.time()
+        model.fit(X_train, y_train)
+        train_time = time.time() - t0
+
+        # predicting time
+        t0 = time.time()
+        preds = model.predict(X_test)
+        pred_time = time.time() - t0
+
+        # cross validation
+        scores = cross_val_score(model, X_train, y_train)
+
+        # append to results
+        results = pd.concat([results, pd.DataFrame({'name': [name],
+                                                    'mean_score': [scores.mean()],
+                                                    'std_score':[scores.std()],
+                                                    'test_accuracy': [accuracy_score(y_test, preds)],
+                                                    'training_time': [train_time],
+                                                    'predicting_time': [pred_time]})
+                             ])
+
+    return results.sort_values(by='test_accuracy', ascending=False)
